495984c9aa15b64e7466afe77e714f9b7a23058c
Stanley LaFond <cslafond@gmail.com>
2019-10-21 11:48:51 -0400

    add import.py to create books table in db

---
+++ import.py
@@ -0,0 +1,25 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# database engine object from SQLAlchemy that manages connections to the database
+engine = create_engine(os.getenv("DATABASE_URL"))
+
+# create a 'scoped session' that ensures different users' interactions with the
+# database are kept separate
+db = scoped_session(sessionmaker(bind=engine))
+
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    for isbn, title, author, year in reader:
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+                {"isbn": isbn, "title": title, "author": author, "year": year})
+        print(f"Added book by {author} title: {title} published in {year}.")
+    db.commit()
+
+if __name__ == "__main__":
+    main()
